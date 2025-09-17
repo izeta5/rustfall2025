b77ea036454531240ad40234d2e198178a75c419
izeta5 <roli.izeta33@gmail.com>
2025-09-17 17:06:26 +0000

    Problem 2 almost

--- assignment_01/src/main.rs
+++ assignment_01/src/main.rs
@@ -10,15 +10,30 @@ fn celsius_to_fahrenheit(c: f64) -> f64{
     return farhenheit
 }
 
-fn main() {
-    println!("Assignment 1");
-    println!("Problem 1. Temperature Converter:");
-    let mut temp: f64 = 35.0;
-    println!("{:.3} C",fahrenheit_to_celsius(temp));
-    for i in (temp as i32 + 1)..(temp as i32 + 6){
-        println!("{:.3} C",fahrenheit_to_celsius(i as f64))
+fn is_even(n: i32) -> bool{
+    if n % 2 == 0{
+        return true
     }
-    println!("Problem 2. Number Analyzer:");
-    
-
+    else{
+        return false
+    }
 }
+
+fn main() {
+    
+    println!("Assignment 1");
+    println!("Problem 1. Temperature Converter:\n");
+    let mut temp: f64 = 35.0;
+    println!("{}F -> {:.3}C\n",temp as i32,fahrenheit_to_celsius(temp));
+    for i in (temp as i32 + 1)..(temp as i32 + 6){
+        println!("{}F -> {:.3}C",i,fahrenheit_to_celsius(i as f64))
+    }
+    
+    println!("\nProblem 2. Number Analyzer:\n");
+    let nums: [i32;10] = [2, 7, 13, 5, 1, 6, 15, 10, 9, 3];
+    for i in 0..nums.len(){
+        println!("{}",is_even(nums[i]));
+    }
+}
+
+//PUT EACH ASSIGNMENT IN A FUNCTION. HAVE MAIN ONLY CALLING 3 FUNCTIONS THATS IT!
