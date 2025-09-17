81e182cbc0a6dffcbfc191cb0880ebc24f5f788d
izeta5 <roli.izeta33@gmail.com>
2025-09-17 18:01:43 +0000

    Problem 2 Done

--- assignment_01/src/main.rs
+++ assignment_01/src/main.rs
@@ -11,7 +11,7 @@ fn celsius_to_fahrenheit(c: f64) -> f64{
 }
 
 fn is_even(n: i32) -> bool{
-    if n % 2 == 0{
+    if (n % 2 == 0) || (n == 0){
         return true
     }
     else{
@@ -19,10 +19,14 @@ fn is_even(n: i32) -> bool{
     }
 }
 
+fn check_guess(guess:i32, secret: i32){
+    
+}
+
 fn main() {
     
     println!("Assignment 1");
-    println!("Problem 1. Temperature Converter:\n");
+    println!("\nProblem 1. Temperature Converter:\n");
     let mut temp: f64 = 35.0;
     println!("{}F -> {:.3}C\n",temp as i32,fahrenheit_to_celsius(temp));
     for i in (temp as i32 + 1)..(temp as i32 + 6){
@@ -30,10 +34,42 @@ fn main() {
     }
     
     println!("\nProblem 2. Number Analyzer:\n");
-    let nums: [i32;10] = [2, 7, 13, 5, 1, 6, 15, 10, 9, 3];
+    let nums: [i32;10] = [2, 7, 13, 5, 1, 6, 15, 0, 9, 3];
     for i in 0..nums.len(){
-        println!("{}",is_even(nums[i]));
+        if is_even(nums[i]) == true{
+            println!("{} is even.",nums[i])
+        }
+        if is_even(nums[i]) == false{
+           if (nums[i] % 3 == 0) && (nums[i] % 5 == 0){
+            println!("FizzBuzz ({})", nums[i])
+            }
+            else if (nums[i] % 3 == 0){
+                println!("Fizz ({})", nums[i])
+            }
+            else if (nums[i] % 5 == 0){
+                println!("Buzz ({})", nums[i])
+            }
+            else{
+                println!("{} is odd.",nums[i])
+            } 
+        }
     }
+    let mut sum:i32 = 0;
+    let mut big:i32 = nums[0];
+    let mut index = 0;
+    while index < nums.len(){
+        if nums[index] > big{
+            big = nums[index];
+        }
+        sum += nums[index];
+        index += 1;
+    }
+    println!("\nFinal Sum = {}",sum);
+    println!("\nLargest Number = {}",big);
+
+    println!("\nProblem 3. Guessing Game:\n");
+    let mut secret_num:i32 = 33;
+
 }
 
 //PUT EACH ASSIGNMENT IN A FUNCTION. HAVE MAIN ONLY CALLING 3 FUNCTIONS THATS IT!
