1f4ba094abc70389851ef76f72447dbb59e4b3c6
izeta5 <roli.izeta33@gmail.com>
2025-10-25 01:03:57 +0000

    Struct Assignment

---
+++ struct_assignment/target/debug/deps/struct_assignment-9e42fc62ecc2996e.d
@@ -0,0 +1,5 @@
+/workspaces/rustfall2025/struct_assignment/target/debug/deps/struct_assignment-9e42fc62ecc2996e.d: src/main.rs
+
+/workspaces/rustfall2025/struct_assignment/target/debug/deps/libstruct_assignment-9e42fc62ecc2996e.rmeta: src/main.rs
+
+src/main.rs:
